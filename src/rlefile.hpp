1d30d336a9483cf99e14cd5d2be3f94b4c055fba
Kuzon1952 <118914404+Kuzon1952@users.noreply.github.com>
2025-04-27 22:08:50 +0300

    Add files via upload
    fixed base32file

--- src/rlefile.hpp
+++ src/rlefile.hpp
@@ -4,8 +4,12 @@
 #include "basefile.hpp"
 
 class RleFile : public BaseFile {
+private: 
+    unsigned char leftover_buffer[256];
+    size_t leftover_count;  
 public:
     using BaseFile::BaseFile;//inherit constructors
+    RleFile(const char* path, const char* mode);
 
     ~RleFile() override;
 
